7d86c8828408e20f732c52ea13d6d9a332c1a8c2
bagus faize <bagusfaize@gmail.com>
2023-10-15 02:11:28 +0700

    add button component

---
+++ src/components/buttons/Button.js
@@ -0,0 +1,17 @@
+import clsx from 'clsx'
+import React from 'react'
+
+export default function DefaultButton({ type = 'solid', children }) {
+  return (
+    <button
+      className={clsx(
+        'focus:outline-none font-medium rounded-lg text-sm px-5 py-2 mb-2 flex items-center space-x-2', {
+        'bg-purple-700 hover:bg-purple-800 text-white focus:ring-4 focus:ring-purple-100': type === 'solid',
+        'text-purple-700 bg-white border border-purple-700 hover:bg-purple-50 focus:ring-4 focus:ring-purple-100': type === 'outline',
+      }
+      )}
+    >
+      {children}
+    </button>
+  )
+}
