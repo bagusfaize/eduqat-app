c21b8a3911360de922a20b8f4e4deb7b7e969059
bagus faize <bagusfaize@gmail.com>
2023-10-15 09:55:21 +0700

    styling

--- src/components/headers/Header.js
+++ src/components/headers/Header.js
@@ -4,7 +4,7 @@ import { HiArrowLongLeft, } from 'react-icons/hi2'
 
 export default function Header({ title, onBack }) {
     return (
-        <div className='p-6 shadow-sm flex items-center'>
+        <div className='bg-white p-6 flex items-center shadow-[0_4px_6px_5px_rgba(0,0,0,0.025)]'>
             <Link href={onBack} className='border-r mx-3 px-3 border-gray-400'>
                 <HiArrowLongLeft className='text-xl' />
             </Link>
